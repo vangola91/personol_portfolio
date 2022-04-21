e5bfc801b7a0b73cff656253a77889fcf27206f5
Aliksandrmax <88476601+vangola91@users.noreply.github.com>
2022-04-22 02:27:11 +0500

    newnew

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'portfolio/home.html')
+    projects=Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects':projects})
