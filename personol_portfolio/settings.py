be4baddbc2fe68621096d5bb1dd4f098f53b5d65
vangola91 <aliksandrmax@yandex.ru>
2022-04-25 17:32:47 +0500

    new local settengs

--- personol_portfolio/settings.py
+++ personol_portfolio/settings.py
@@ -122,6 +122,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+try:
+    from .local_settings import  *
+except ImportError:
+    print("Looks link no local file. You must be on ")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
